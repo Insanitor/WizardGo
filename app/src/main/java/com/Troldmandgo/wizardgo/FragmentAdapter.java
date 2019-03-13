7c90d745d97ace87c540d3a2abfbdcd4cbcb4cf8
Dennis Dupont <TheInsanitor@hotmail.com>
2019-03-13 11:57:47 +0100

    Added FragmentAdapter Added Several Fragments for Login Activity Added LoginPresenter

---
+++ app/src/main/java/com/Troldmandgo/wizardgo/FragmentAdapter.java
@@ -0,0 +1,34 @@
+package com.Troldmandgo.wizardgo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+
+    final List<Fragment> mFragmentList = new ArrayList<>();
+    final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title)
+    {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
---
+++ app/src/main/java/com/Troldmandgo/wizardgo/LoginCreateUserFragment.java
@@ -0,0 +1,21 @@
+package com.Troldmandgo.wizardgo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginCreateUserFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_createuser, container, false);
+
+        return view;
+    }
+}
