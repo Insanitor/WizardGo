e1b26bc1f514a5d3c99b9145114e94faf987bb77
Insanitor <theinsanitor@hotmail.com>
2019-03-28 11:56:26 +0100

    Did stuff

--- app/src/main/java/com/Troldmandgo/wizardgo/GameScreen/Game_Activity.java
+++ app/src/main/java/com/Troldmandgo/wizardgo/GameScreen/Game_Activity.java
@@ -1,20 +1,43 @@
 package com.Troldmandgo.wizardgo.GameScreen;
 
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.Troldmandgo.wizardgo.Login.Login_Activity;
 import com.Troldmandgo.wizardgo.R;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-public class Game_Activity extends AppCompatActivity {
+import java.util.List;
+
+public class Game_Activity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
 
     private final String ACCESS_KEY = "pk.eyJ1IjoiaW5zYW5pdG9yIiwiYSI6ImNqdGZxNXdtMTBjb3o0YW1oaXh2bXJnbmwifQ.AXEmfNbBF89KZZ1kcW0Dxw";
     private MapView mapView;
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +50,97 @@ public class Game_Activity extends AppCompatActivity {
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.LIGHT, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
+        mapView.getMapAsync(this);
+        CameraPosition position = new CameraPosition.Builder()
+                .target(new LatLng(11.78,55.444901))
+                .zoom(10)
+                .build();
 
-                    }
-                });
+    }
+
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        Game_Activity.this.map = mapboxMap;
+        map.setStyle(Style.LIGHT, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
+
+                //Adds a marker with no functionality
+                Feature myMarker;
+                myMarker = Feature.fromGeometry(Point.fromLngLat(11.78, 55.444901));
+                style.addSource(new GeoJsonSource("LayerSource", FeatureCollection.fromFeature(myMarker)));
+                style.addImage("MyImage", BitmapFactory.decodeResource(Game_Activity.this.getResources(), R.drawable.mapbox_marker_icon_default));
+                style.addLayer(new SymbolLayer("MarkerLayer", "LayerSource")
+                        .withProperties(PropertyFactory.iconImage("MyImage")));
+
+
+//                Adds a marker with an onclick-event and a popup message
+                map.addMarker(new MarkerOptions()
+                        .position(new LatLng(11.78, 55.444901))
+                        .title("Eiffel Tower"));
+
+
             }
         });
     }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        //Check if permissions are granted
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            //Get an instance of the component
+            LocationComponent locationComponent = map.getLocationComponent();
+            LocationComponentOptions options = LocationComponentOptions.builder(this)
+
+                    .foregroundDrawable(R.drawable.ic_wizard)
+                    .backgroundDrawable(R.drawable.ic_wizard)
+                    .foregroundDrawableStale(R.drawable.ic_wizard)
+                    .backgroundDrawableStale(R.drawable.ic_wizard)
+                    .accuracyAlpha(0)
+                    .compassAnimationEnabled(true)
+                    .accuracyAnimationEnabled(true)
+                    .compassAnimationEnabled(true)
+                    .minZoomIconScale(1f)
+                    .maxZoomIconScale(1.5f)
+                    .build();
+
+            //Activate with options
+            locationComponent.activateLocationComponent(this, loadedMapStyle, options);
+
+            //Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            //Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+
+            //Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_message, Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            map.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
 }
+
